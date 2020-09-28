828a6b50f573cead6fb79fda9742e2e3f6167a0f
mihalichpalich <pavlovtfspb@gmail.com>
2020-09-28 15:24:27 +0300

    Updates

---
+++ precache-manifest.329dad229cd56c39ce33e9e5a43f4883.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35dbd20c98c50b01ce0d815a4c0297f",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "ff6ff4885dbd4a7175bd",
+    "url": "/react_portfolio/static/css/main.07a40a77.chunk.css"
+  },
+  {
+    "revision": "0687c04ef8666936765a",
+    "url": "/react_portfolio/static/js/2.d01aba1f.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/react_portfolio/static/js/2.d01aba1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff6ff4885dbd4a7175bd",
+    "url": "/react_portfolio/static/js/main.e33a86ed.chunk.js"
+  },
+  {
+    "revision": "5397e927921dde71372c",
+    "url": "/react_portfolio/static/js/runtime-main.d05fa38a.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/react_portfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/react_portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "526ad340dbc3cc92f4efd6e939ed6b29",
+    "url": "/react_portfolio/static/media/react-apollo-course-frontend.526ad340.jpg"
+  },
+  {
+    "revision": "ca1fecf0cc237fce79984a63a13705ed",
+    "url": "/react_portfolio/static/media/react-chat.ca1fecf0.jpg"
+  },
+  {
+    "revision": "5e38b9c287a03411069238387defd65c",
+    "url": "/react_portfolio/static/media/react-native-flights-app.5e38b9c2.png"
+  },
+  {
+    "revision": "152c3e011d0134a1a6fc020d2f947210",
+    "url": "/react_portfolio/static/media/react-native-tutor-app.152c3e01.jpg"
+  },
+  {
+    "revision": "ab273de43722b7986fbfb36e3465c29a",
+    "url": "/react_portfolio/static/media/react_chat.ab273de4.jpg"
+  }
+]);
